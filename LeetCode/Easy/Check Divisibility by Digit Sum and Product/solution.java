3baea273e2fd88a8fe506605913b12087c92ea51
JeeT-149 <divyajeetsahu24@gmail.com>
2026-08-22 15:48:14 +0530

    GitSync: leetcode - Check Divisibility by Digit Sum and Product

--- LeetCode/Easy/Check Divisibility by Digit Sum and Product/solution.java
+++ LeetCode/Easy/Check Divisibility by Digit Sum and Product/solution.java
@@ -4,7 +4,12 @@ class Solution {
         int digitsum = 0;
         int digitproduct = 1;
         while (temp>0){
-            int digi
+            int digit = temp%10;
+            digit+=digit;
+            digitproduct*=digit;
+            temp/=10;
         }
+        int total = digitsum+digitproduct;
+        r
     }
 }
