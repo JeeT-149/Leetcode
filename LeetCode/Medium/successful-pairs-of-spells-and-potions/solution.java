df96ceec1b743ac8bce2ced4bfa5d29d4763e7da
JeeT-149 <divyajeetsahu24@gmail.com>
2026-08-29 18:05:37 +0530

    GitSync: leetcode - Successful Pairs of Spells and Potions

---
+++ LeetCode/Medium/successful-pairs-of-spells-and-potions/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] pairs = new int[n];
+        Arrays.sort(potions);
+        for (int i = 0; i<n ; i++){
+            long target = (success+spells[i]-1)/spells[i];
+            int low = 0;
+            int high = m-1;
+            int firstvalid = m;
+            while (low<=high){
+                int mid = low + (high-low)/2;
+                if (potions[mid]>= target){
+                    firstvalid = mid;
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            pairs[i]=m-firstvalid;
+        }
+        return pairs;
+    }
+}
