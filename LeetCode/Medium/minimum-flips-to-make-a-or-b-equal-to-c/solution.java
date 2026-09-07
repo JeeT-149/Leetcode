da20a594647adb5a1255b15a0ec5a41ee3fa582c
JeeT-149 <divyajeetsahu24@gmail.com>
2026-09-07 17:19:23 +0530

    GitSync: leetcode - Minimum Flips to Make a OR b Equal to c

---
+++ LeetCode/Medium/minimum-flips-to-make-a-or-b-equal-to-c/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int flips = 0;
+        while(a>0 || b>0 || c>0){
+            int bita = a&1;
+            int bitb = b&1;
+            int bitc = c&1;
+            if (bitc==1){
+                if (bita == 0 && bitb == 0){
+                    flips++;
+                }
+            }
+                else{
+                    flips += (bita+bitb);
+                }
+                a>>>=1;
+                b>>>=1;
+                c>>>=1;
+        }
+        return flips;
+    }
+}
